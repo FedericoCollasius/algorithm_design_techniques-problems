6208376ec291d12b1961bf93b1f5b13316de89a9
Federico Collasius <fede.collasius@gmail.com>
2025-04-17 13:50:20 -0300

    hilocomunicadosdone

--- tp4/hilocomunicados.cpp
+++ tp4/hilocomunicados.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <vector>
+#include <iomanip>
+
 
 using namespace std;
 
+// El caso en el que no hay ? no me interesa porque ahi directamente puedo contar 
+// y se si llego o no. 
+
+// +-+++-
+// ???+-+
+
+// ++-,-++,+-+
+
+// target = 1
+// cant_preg = 3
+
+// bt(2, 3)
+// plus = bt(3, 2)
+
+// minus = bt(2, 2)
+
+int bt(int target, int cant_preg) {
+    if (target == 0 && cant_preg == 0) {
+      return 1;
+    }
+
+    if (cant_preg == 0) {
+      return 0;
+    }
+
+    if (cant_preg > 0) {
+      int plus = bt(target+1, cant_preg-1);
+      int minus = bt(target-1, cant_preg-1);
+      return plus + minus;
+    } 
+}
+
+
 int main () {
-  double res = 0.0;
+  long double res = 0.0;
   int target = 0;
   int cant_preg = 0;
   string s1 = ""; string s2 = "";
@@ -28,13 +64,17 @@ int main () {
       cant_preg++;
     }
   }
-  target*=(-1);
+  int sols;
+  if (target == 0 && cant_preg == 0) {
+    res = 1;
+  } else if (target != 0 && cant_preg == 0) {
+    res = 0;
+  } else {
+    sols = bt(target, cant_preg);
+    res = sols / (pow(2, cant_preg));
+  }
 
-  long int espacio_total = pow(2,cant_preg);
-
-
-
-  cout << target << endl;
-
+  cout << fixed << setprecision(12) << res << endl;
   return 0;
 }
+
