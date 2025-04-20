758eca389bb1e5ba78f637d8a2fcb09b5ab0812c
FedericoCollasius <fede.collasius@gmail.com>
2025-04-20 13:03:26 -0300

    finish cambiandose_de_aula

--- tp4/cambiandose_de_aula.cpp
+++ tp4/cambiandose_de_aula.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
 #include <iomanip>
 
 using namespace std;
@@ -14,33 +14,112 @@ void printMatrix(const vector<vector<int>>& M) {
     }
 }
 
-int aula_bt(const vector<vector<int>>& M, ) {
-    if 
+int BOTTOM = 1000000;
+int n;
+int m;
+
+vector<vector<int>> MAX;
+int DPMax(int x, int y, vector<vector<int>>& aulas) {
+    cout << "(" << x << "," << y << "): Entre a la funcion" << endl;
+    int curr_aula = aulas[x][y];
+
+    if (MAX[x][y] == BOTTOM) {
+        int res = 0;
+
+        if (x == n-1 && y == m-1) {
+            cout << "(" << x << "," << y << "): Llegamos al limite de la matriz" << endl;
+            res = curr_aula;
+        
+        } else if (y == m-1) {
+            cout << "(" << x << "," << y << "): Llegue al limite derecho, me muevo hacia abajo" << endl;
+            res = curr_aula + DPMax(x + 1, y, aulas);
+        
+        } else if (x == n-1) {
+            cout << "(" << x << "," << y << "): Llegue al limite inferior, me muevo hacia la derecha" << endl;
+            res = curr_aula + DPMax(x, y + 1, aulas);
+        
+        } else {
+            int abajo = DPMax(x + 1, y, aulas);
+            int derecha = DPMax(x, y + 1, aulas);
+            res = curr_aula + max(abajo, derecha);
+        }
+
+        MAX[x][y] = res;
+    }
+    cout << "(" << x << "," << y << "): Devuelvo " << MAX[x][y] << endl;
+    return MAX[x][y];
+}
+
+vector<vector<int>> MIN;
+int DPMin(int x, int y, vector<vector<int>>& aulas) {
+    cout << "(" << x << "," << y << "): Entre a la funcion" << endl;
+    int curr_aula = aulas[x][y];
+
+    if (MIN[x][y] == BOTTOM) {
+        int res = 0;
+
+        if (x == n-1 && y == m-1) {
+            cout << "(" << x << "," << y << "): Llegamos al limite de la matriz" << endl;
+            res = curr_aula;
+        
+        } else if (y == m-1) {
+            cout << "(" << x << "," << y << "): Llegue al limite derecho, me muevo hacia abajo" << endl;
+            res = curr_aula + DPMin(x + 1, y, aulas);
+        
+        } else if (x == n-1) {
+            cout << "(" << x << "," << y << "): Llegue al limite inferior, me muevo hacia la derecha" << endl;
+            res = curr_aula + DPMin(x, y + 1, aulas);
+        
+        } else {
+            int abajo = DPMin(x + 1, y, aulas);
+            int derecha = DPMin(x, y + 1, aulas);
+            res = curr_aula + min(abajo, derecha);
+        }
+
+        MIN[x][y] = res;
+    }
+    cout << "(" << x << "," << y << "): Devuelvo " << MIN[x][y] << endl;
+    return MIN[x][y];
 }
 
 int main() {
     int tests;
     cin >> tests;
-    //cout << "Tests: " << tests << endl;
+    cout << "Tests: " << tests << endl;
 
     for (int k = 0; k < tests; k++) {
-        int n, m;
         cin >> n >> m;
-        //cout << "n: " << n << " m: " << m << endl;
+        cout << "n:" << n << " m:" << m << endl;
 
-        vector<vector<int>> M(n, vector<int>(m));
-
+        if ((n + m - 1) % 2) {
+            cout << "NO" << endl;
+            continue;
+        }
+        
+        vector<vector<int>> aulas(n, vector<int>(m));
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++){
-                cin >> M[i][j];
-                //cout << "pos: " << M[i][j] << endl;
+                cin >> aulas[i][j];
             }
         }
+        cout << "La distribucion de aulas del ejercicio actual es: " << endl;
+        printMatrix(aulas);
 
-        //cout << "Matriz " << k+1 << ":" << endl;
-        //printMatrix(M);
-        int res = aula_bt();
-        //cout << res << endl;
+        MAX.assign(n, vector<int>(m, BOTTOM));
+        int res_max = DPMax(0, 0, aulas);
+        cout << "MAX:" << endl;
+        printMatrix(MAX);
+        
+        MIN.assign(n, vector<int>(m, BOTTOM));
+        int res_min = DPMin(0, 0, aulas);
+        cout << "MIN:" << endl;
+        printMatrix(MIN);
+
+        if (res_min <= 0 && res_max >= 0) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
     }
 
     return 0; 
